291345c41a018b50493240ff3cb5722078b331b3
Elia <s338009@studenti.polito.it>
2025-11-10 18:16:46 +0100

    Map created in the Homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,11 +1,23 @@
 import { Container } from 'react-bootstrap'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function HomePage() {
 
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center text-center">
+    /*<Container className="d-flex flex-column justify-content-center align-items-center text-center">
       <h2>Hello</h2>
-    </Container>
+    </Container>*/
+    <MapContainer center={[45.0703, 7.6869]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[45.0703, 7.6869]}>
+        <Popup>
+          Piazza Castello  <br /> Centro di Torino.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
